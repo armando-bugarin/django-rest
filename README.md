f44768540454802f56b77d94b15e2426a149797d
Armando Bugarin <armando.bugarin24@gmail.com>
2024-02-19 22:55:55 -0800

    edit files

---
+++ README.md
@@ -0,0 +1,17 @@
+# Lab - Class 31
+
+# Project: django-rest
+
+# Author: Armando Bugarin
+
+### Dependencies installed
+
+- `pip install pytest-watch`
+- `pip install django`
+- `pip install djangorestframework`
+
+### Tests
+
+- How do you run tests? `python manage.py test`
+- How to run server? `python manage.py runserver`
+- Any tests of note?
--- drf-api/rest/views.py
+++ drf-api/rest/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView
+from .models import Rest
+from .serializers import RestSerializer
 
 # Create your views here.
+
+class RestList(ListCreateAPIView):
+    # Anything that inherits from ListAPI View is going to need 2 things.
+    # What is the collection of things, aka the queryset
+    queryset = Rest.objects.all()
+
+    #serializing
+    serializer_class = RestSerializer
+
+# The ThingDetail needs the same things
+class RestDetail(RetrieveUpdateDestroyAPIView):
+    queryset = Rest.objects.all()
+    serializer_class = RestSerializer
